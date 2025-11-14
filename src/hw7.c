c54cb9d0ab18fa4024697c269af69ac0c43bd119
Hamim Seam <hamim.seam@stonybrook.edu>
2025-11-14 01:39:45 -0500

    BST and postfix conversion

--- src/hw7.c
+++ src/hw7.c
@@ -2,14 +2,47 @@
 #include <stdlib.h>
 
 bst_sf* insert_bst_sf(matrix_sf *mat, bst_sf *root) {
-    return NULL;
+    bst_sf *new_node = malloc(sizeof(bst_sf));
+    new_node->mat = mat;
+    new_node->left_child = NULL;
+    new_node->right_child = NULL;
+
+    bst_sf *parent = NULL;
+    bst_sf *curr = root;
+
+    while (curr) {
+        parent = curr;
+        if (mat->name < curr->mat->name) curr = curr->left_child;
+        else curr = curr->right_child;
+    }
+    
+    if (!parent) root = new_node;
+    else if (mat->name < parent->mat->name) parent->left_child = new_node;
+    else parent->right_child = new_node;
+
+    return root;
 }
 
 matrix_sf* find_bst_sf(char name, bst_sf *root) {
+    bst_sf *node = root;
+
+    while (node) {
+        char node_name = node->mat->name;
+        if (name == node_name) return node->mat;
+        else if (name < node_name) node = node->left_child;
+        else node = node->right_child;
+    }
+
     return NULL;
 }
 
 void free_bst_sf(bst_sf *root) {
+    if (root) {
+        free_bst_sf(root->left_child);
+        free_bst_sf(root->right_child);
+        free(root->mat);
+        free(root);
+    }
 }
 
 matrix_sf* add_mats_sf(const matrix_sf *mat1, const matrix_sf *mat2) {
@@ -105,12 +138,108 @@ matrix_sf* create_matrix_sf(char name, const char *expr) {
     return res;
 }
 
+int prec(char operator) {
+    switch (operator) {
+        case '\'': return 3;
+        case '*': return 2;
+        case '+': return 1;
+        default: return -1;
+    }
+}
+
 char* infix2postfix_sf(char *infix) {
-    return NULL;
+    int len = strlen(infix);
+    char *postfix = malloc(len + 1);
+    int postfix_end = 0;
+
+    char *stack = malloc(len + 1);
+    int top = -1;
+
+    for (int i = 0; i < len; i++) {
+        char c = infix[i];
+
+        if (isalpha(c)) postfix[postfix_end++] = c;
+
+        else if (c == '(') stack[++top] = c;
+
+        else if (c == ')') {
+            while (top >= 0 && stack[top] != '(') 
+                postfix[postfix_end++] = stack[top--];
+            
+            top--;
+        } 
+        
+        else {
+            while (top >= 0 && prec(c) <= prec(stack[top])) 
+                postfix[postfix_end++] = stack[top--];
+            
+            stack[++top] = c;
+        }
+    }
+
+    while (top >= 0) {
+        postfix[postfix_end++] = stack[top--];
+    }
+    postfix[postfix_end] = '\0';
+
+    free(stack);
+    return postfix;
 }
 
 matrix_sf* evaluate_expr_sf(char name, char *expr, bst_sf *root) {
-    return NULL;
+    char* postfix = infix2postfix_sf(expr);
+    int postfix_len = strlen(postfix);
+    matrix_sf** stack = malloc(postfix_len * sizeof(matrix_sf*));
+    int top = -1;
+
+    matrix_sf* operand1;
+    matrix_sf* operand2;
+
+    for (int i = 0; i < postfix_len; i++) {
+        char c = postfix[i];
+        switch (c) {
+            case '\'': 
+                operand1 = stack[top--];
+                stack[++top] = transpose_mat_sf(operand1);
+                
+                if (!isalpha(operand1->name)) free(operand1);
+
+                stack[top]->name = '?';
+                break;
+
+            case '*':
+                operand1 = stack[top--];
+                operand2 = stack[top--];
+                stack[++top] = mult_mats_sf(operand1, operand2);
+
+                if (!isalpha(operand1->name)) free(operand1);
+                if (!isalpha(operand2->name)) free(operand2);
+
+                stack[top]->name = '?';
+                break;
+
+            case '+':
+                operand1 = stack[top--];
+                operand2 = stack[top--];
+                stack[++top] = add_mats_sf(operand1, operand2);
+
+                if (!isalpha(operand1->name)) free(operand1);
+                if (!isalpha(operand2->name)) free(operand2);
+
+                stack[top]->name = '?';
+                break;
+
+            default:
+                operand1 = find_bst_sf(c, root);
+                stack[++top] = operand1;
+                break;
+        }
+    }
+
+    matrix_sf* res = stack[0];
+    free(stack);
+    free(expr);
+    return res;
 }
 
 matrix_sf *execute_script_sf(char *filename) {
