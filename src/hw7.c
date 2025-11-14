749f17cf1a695136b72a3eb79f4dfd9a28f5fbae
Hamim Seam <hamim.seam@stonybrook.edu>
2025-11-13 20:20:28 -0500

    Implement add, multiply, transpose, and create matrix

--- src/hw7.c
+++ src/hw7.c
@@ -1,4 +1,5 @@
 #include "hw7.h"
+#include <stdlib.h>
 
 bst_sf* insert_bst_sf(matrix_sf *mat, bst_sf *root) {
     return NULL;
@@ -12,19 +13,96 @@ void free_bst_sf(bst_sf *root) {
 }
 
 matrix_sf* add_mats_sf(const matrix_sf *mat1, const matrix_sf *mat2) {
-    return NULL;
+    int num_rows = mat1->num_rows;
+    int num_cols = mat1->num_cols;
+
+    int res_len = num_rows * num_cols;
+    matrix_sf *res = malloc(sizeof(matrix_sf) + res_len * sizeof(int));
+    res->num_rows = num_rows;
+    res->num_cols = num_cols;
+
+    for (int i = 0; i < res_len; i++) {
+        res->values[i] = mat1->values[i] + mat2->values[i];
+    }
+
+    return res;
 }
 
 matrix_sf* mult_mats_sf(const matrix_sf *mat1, const matrix_sf *mat2) {
-   return NULL;
+    int m = mat1->num_rows;
+    int n = mat2->num_cols;
+    int r = mat1->num_cols;
+
+    int res_len = m * n;
+    matrix_sf *res = calloc(1, sizeof(matrix_sf) + res_len * sizeof(int));
+    res->num_rows = m;
+    res->num_cols = n;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < r; k++) {
+                res->values[i * n + j] +=
+                    mat1->values[i * r + k] * mat2->values[k * n + j];
+            }
+        }
+    }
+
+    return res;
 }
 
 matrix_sf* transpose_mat_sf(const matrix_sf *mat) {
-    return NULL;
+    int m = mat->num_rows;
+    int n = mat->num_cols;
+
+    int res_len = n * m;
+    matrix_sf *res = malloc(sizeof(matrix_sf) + res_len * sizeof(int));
+    res->num_rows = n;
+    res->num_cols = m;
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            res->values[j * m + i] = mat->values[i * n + j];
+        }
+    }
+
+    return res;
 }
 
 matrix_sf* create_matrix_sf(char name, const char *expr) {
-    return NULL;
+    char* s = expr;
+
+    int m = strtol(s, &s, 10);
+    int n = strtol(s, &s, 10);
+
+    int res_len = m * n;
+    matrix_sf *res = malloc(sizeof(matrix_sf) + res_len * sizeof(int));
+    res->name = name;
+    res->num_rows = m;
+    res->num_cols = n;
+
+    int i = 0;
+    int j = 0;
+    while (*s == ' ') s++; // s points to '['
+    s++; // s points past '['
+
+    char* end = s;
+    while (*s != ']') {
+        while (*s == ' ') s++;
+
+        int k = strtol(s, &end, 10);
+        if (end == s && *s == ';') {
+            i++;
+            j = 0;
+            s = end + 1;
+        } else if (end != s) {
+            res->values[i * n + j] = k;
+            s = end;
+            j++;
+        }
+
+    }
+
+    return res;
 }
 
 char* infix2postfix_sf(char *infix) {
